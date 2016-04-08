6e65e0d5d28d60cb4554373183bce72127ff047c
student <student@debian>
2016-04-08 12:24:12 +0200

    Git init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Card.h"
+
+using namespace std;
+
+int main()
+{
+Card cards[52];	
+
+for(int i=0;i<52;i++)
+cout<<cards[i].description()<<endl;
+//cout<<"Elo"<<endl;
+	
+}
